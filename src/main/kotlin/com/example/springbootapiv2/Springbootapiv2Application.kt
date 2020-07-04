ebd7056494f94040feacef6b6301c424da821310
wonsang.hwang <wonsang.hwang@navercorp.com>
2020-07-05 06:47:07 +0900

    make init setting

---
+++ src/main/kotlin/com/example/springbootapiv2/Springbootapiv2Application.kt
@@ -0,0 +1,11 @@
+package com.example.springbootapiv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springbootapiv2Application
+
+fun main(args: Array<String>) {
+    runApplication<Springbootapiv2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootapiv2/Springbootapiv2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootapiv2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Springbootapiv2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
