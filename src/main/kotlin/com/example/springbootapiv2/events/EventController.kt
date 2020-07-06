6b56aa1b816fd798468c82094692ab7e6f967658
wonsang.hwang <wonsang.hwang@navercorp.com>
2020-07-06 15:03:56 +0900

    @RequestMapping을 이용하여 class단에서 어노테이션 사용하도록 수정처리

--- src/main/kotlin/com/example/springbootapiv2/events/EventController.kt
+++ src/main/kotlin/com/example/springbootapiv2/events/EventController.kt
@@ -1,23 +1,27 @@
 package com.example.springbootapiv2.events
 
+import org.springframework.hateoas.MediaTypes
 import org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.hateoas.server.mvc.linkTo
+import org.springframework.web.bind.annotation.RequestMapping
 import java.net.URI
 
 @Controller
+@RequestMapping(value = ["/api/events"], produces = [MediaTypes.HAL_JSON_VALUE])
 class EventController {
 
-    @PostMapping("/api/events")
+    @PostMapping
     fun createEvent(@RequestBody event: Event): ResponseEntity<Event> {
+
+        event.id = 10
 
         val createdUri: URI = linkTo(EventController::class.java)
                 .slash("{id}")
                 .toUri()
-
-        event.id = 10
 
         // URI return을 원하는 경우 아래 return값 사용
         //return ResponseEntity.created(createdUri).build()
