d984a86d51bbd4a7331d56dfa5c5b356ca2ec6d6
wonsang.hwang <wonsang.hwang@navercorp.com>
2020-07-06 12:38:46 +0900

    createEvent 생성을 URI가 아닌 Event class로 할 경우

--- src/main/kotlin/com/example/springbootapiv2/events/EventController.kt
+++ src/main/kotlin/com/example/springbootapiv2/events/EventController.kt
@@ -1,22 +1,26 @@
 package com.example.springbootapiv2.events
 
-import org.springframework.hateoas.server.mvc.ControllerLinkBuilder.methodOn
 import org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import java.net.URI
 
 @Controller
 class EventController {
 
     @PostMapping("/api/events")
-    fun createEvent(): ResponseEntity<URI> {
+    fun createEvent(@RequestBody event: Event): ResponseEntity<Event> {
 
-        val createdUri: URI = linkTo(methodOn(EventController::class.java).createEvent())
+        val createdUri: URI = linkTo(EventController::class.java)
                 .slash("{id}")
                 .toUri()
 
-        return ResponseEntity.created(createdUri).build()
+        event.id = 10
+
+        // URI return을 원하는 경우 아래 return값 사용
+        //return ResponseEntity.created(createdUri).build()
+        return ResponseEntity.created(createdUri).body(event)
     }
 }
--- src/test/kotlin/com/example/springbootapiv2/events/EventControllerTests.kt
+++ src/test/kotlin/com/example/springbootapiv2/events/EventControllerTests.kt
@@ -1,5 +1,6 @@
 package com.example.springbootapiv2.events
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -9,19 +10,39 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import java.time.LocalDateTime
+
 @WebMvcTest // 슬라이싱테스
 class EventControllerTests {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var objectMapper: ObjectMapper // content를 JSON으로 변경하기 위해서 사용
+
     @Test
     fun createEvent() {
+
+        var event: Event = Event(name = "spring",
+                description = "REST API Development with Spring",
+                beginEnrollmentDateTime = LocalDateTime.of(2018, 11, 23, 14, 21),
+                closeEnrollmentDateTime = LocalDateTime.of(2018, 11, 24, 14, 21),
+                beginEventDateTime = LocalDateTime.of(2018, 11, 28, 14, 21),
+                endEventDateTime = LocalDateTime.of(2018, 11, 26, 14, 21),
+                basePrice = 100,
+                maxPrice = 200,
+                limitOfEnrollment = 100,
+                location = "강남역 D2 스타텁 팩토리")
+
         mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaTypes.HAL_JSON))
+                .accept(MediaTypes.HAL_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
                 .andExpect(status().isCreated) // 201 응답이 create
+                .andExpect(jsonPath("id").exists())
     }
 
 }
